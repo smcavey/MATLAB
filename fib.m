23cd6acb9d0d9712d7c73273621e9796528caf91
Spencer McAvey <50082193+smcavey@users.noreply.github.com>
2021-01-19 12:59:13 -0500

    Update fib.m

--- fib.m
+++ fib.m
@@ -1,7 +1,8 @@
+% calculate and print first 10 numbers in fibonacci sequence
 N = 10;
 f(1) = 1;
 f(2) = 1;
 for n = 3:N
-    f(n) = f(n-1) + f(n-2)
+    f(n) = f(n-1) + f(n-2) % fibonacci number is the sum of two prior numbers in the sequence
 end
-f(1:10)
+f(1:10) % print first 10 numbers in fibonacci sequence
